5885faa6705ee725eb2a4259419fcd85ce5ace81
Mark Cunningham <freelance.webdev.iow@gmail.com>
2025-01-09 12:38:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  new Glide('.glide', {
+  const glide = new Glide('.glide', {
     type: 'carousel', // Carousel mode
     startAt: 0,       // Start at the first slide
     perView: 3,       // Show 3 slides at a time
@@ -9,5 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
       1024: { perView: 2 },
       600: { perView: 1 }
     }
-  }).mount();
+  });
+
+  // Mount Glide only after slides are added
+  glide.mount();
 });
